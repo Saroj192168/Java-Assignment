4f0551ad747f5a588bd2fe0185d47d56f7c872b5
saroj192168 <saroj192168@gmail.com>
2023-07-20 17:31:30 +0545

    Intial Server and Dependency Setup

---
+++ src/main/java/com/JavaInternshipAssignmnet/demo/JavaInternshipAssignmnetApplication.java
@@ -0,0 +1,13 @@
+package com.JavaInternshipAssignmnet.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaInternshipAssignmnetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaInternshipAssignmnetApplication.class, args);
+	}
+
+}
